4040b80b24bbbb76a6fc5c3ec0c672e681e94e1d
musesum <musesum@gmail.com>
2023-09-09 12:07:46 -0700

    platforms: [.iOS(.v15)]

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "MuFlo",
-    platforms: [.iOS(.v16)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "MuFlo",
