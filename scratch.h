a7938eb303ea3aba6f80791c3655aca5188389bd
musesum <musesum@gmail.com>
2025-04-23 17:20:15 -0700

    Exported types

--- scratch.h
+++ scratch.h
@@ -1,188 +1,33 @@
- 0.00: sky ✓
- 0.16: pipe.avp ✓
- 0.23: canvas ✓
- 0.28: plato.avp ✓
- 0.34: cell ✓
- 0.40: camera ✓
- 0.45: more ✓
- 0.52: hand ✓
- 0.70: midi ✓
-MIDI.swift:init():52:Initializing MIDI (MIDI.swift:init():52)
- 2.62: 🧭 updateTexture(3840,2160) pipe.draw.out
- 2.62: 🧭 updateTexture(3840,2160) pipe.cell.out
- 2.63: 🧭 updateTexture(3840,2160) pipe.color.out
- 2.63: 🧭 updateTexture(256,1) pipe.color.pal
- 2.63: 🧭 updateTexture(3840,2160) pipe.camix.out
- 2.64: 🧭 updateTexture(3840,2160) pipe.tile.out
- 16.69: 🧭 customTexture(_:_:remake:) via: map.cube.cudex
- 16.69: 🧭 TouchesView::init size (3840,2160)
- 16.70: 🖐️left.ring.knuc(on: false)
- 16.70: 🖐️left.little.tip(on: true)
- 16.70: 🖐️left.little.base(on: false)
- 16.70: 🖐️left.middle.inter(on: false)
- 16.70: 🖐️left.little.knuc(on: false)
- 16.70: 🖐️left.middle.base(on: false)
- 16.70: 🖐️left.index.inter(on: false)
- 16.70: 🖐️left.ring.meta(on: false)
- 16.70: 🖐️left.index.base(on: false)
- 16.70: 🖐️left.middle.meta(on: false)
- 16.70: 🖐️left.thumb.knuc(on: false)
- 16.70: 🖐️left.thumb.base(on: false)
- 16.70: 🖐️left.little.meta(on: false)
- 16.70: 🖐️left.ring.inter(on: false)
- 16.70: 🖐️left.index.knuc(on: false)
- 16.70: 🖐️left.little.inter(on: false)
- 16.70: 🖐️left.thumb.inter(on: false)
- 16.70: 🖐️hand.left.wrist(on: false)
- 16.70: 🖐️left.ring.tip(on: true)
- 16.70: 🖐️left.index.meta(on: false)
- 16.70: 🖐️left.middle.knuc(on: false)
- 16.70: 🖐️left.index.tip(on: true)
- 16.70: 🖐️left.ring.base(on: false)
- 16.70: 🖐️hand.left.forearm(on: false)
- 16.70: 🖐️left.middle.tip(on: true)
- 16.70: 🖐️left.thumb.tip(on: true)
- 16.70: 🖐️right.thumb.base(on: false)
- 16.70: 🖐️right.middle.base(on: false)
- 16.70: 🖐️right.little.base(on: false)
- 16.70: 🖐️right.index.tip(on: true)
- 16.70: 🖐️right.little.tip(on: true)
- 16.70: 🖐️right.little.knuc(on: false)
- 16.70: 🖐️hand.right.wrist(on: false)
- 16.70: 🖐️right.index.base(on: false)
- 16.70: 🖐️right.middle.knuc(on: false)
- 16.70: 🖐️right.index.inter(on: false)
- 16.70: 🖐️right.thumb.inter(on: false)
- 16.70: 🖐️right.thumb.tip(on: true)
- 16.70: 🖐️right.middle.inter(on: false)
- 16.70: 🖐️right.ring.tip(on: true)
- 16.70: 🖐️right.middle.meta(on: false)
- 16.70: 🖐️right.ring.meta(on: false)
- 16.70: 🖐️right.index.knuc(on: false)
- 16.70: 🖐️right.ring.base(on: false)
- 16.70: 🖐️right.little.inter(on: false)
- 16.70: 🖐️right.thumb.knuc(on: false)
- 16.70: 🖐️right.ring.knuc(on: false)
- 16.70: 🖐️right.little.meta(on: false)
- 16.70: 🖐️right.ring.inter(on: false)
- 16.70: 🖐️right.middle.tip(on: true)
- 16.70: 🖐️hand.right.forearm(on: false)
- 16.70: 🖐️right.index.meta(on: false)
- 17.13: 📋 updateUIView touchesView(0,0, 0,0)
- 17.18: 🧭 resizeFrame(3840,2160) clipNorm( 0.00, 0.00,  1.00, 1.00)
- 17.18: 🧭 Non-Immersive size(1280,720) ports:0
-
- 43.61: 📋 updateUIView touchesView(0,0, 0,0)
- 43.64: 📋 updateUIView touchesView(0,0, 0,0)
- 43.64: 🧭 resizeFrame(2732,2048) clipNorm( 0.12, 0.00,  0.75, 1.00)
- 43.64: 🧭 Immersive size(910,682) ports:0
- 43.67: 🧭 Immersive config
-
- " UserInfo={NSLocalizedDescription=Unable to find function "CC_ApplyStencilFragmentFunction" (render pipeline) in binary archives. 
- Expected <MTLBinaryKey: 0x30273d0d0> Key: 
- 1E6372F4FC948C5F69FA1A73831B9C97680E498FF3C029E29E91A81E00F564C8 Function hash
- 0000000000000000000000000000000000000000000000000000000000000000 air-nt 
- 94C247712038C4872E8E59EC07EC249E3916F0410439854EF64AD2069EB8875E air-nt plugin
- 7551C170C80655A98DAA04E12D5F3DBBF851B1F2515D8580188E7A4269B581E8 metal framework 
- }
-cannot add handler to 0 from 1 - dropping
- 43.92: 👍♦️ ✋ left.index.tip  (-0.06,-0.48,-0.13) ∆ thumbTip(-0.07,-0.46,-0.14) =>  0.029 👐1009 0 => 3
- 43.92: 👐 from: left.middle.tip hash: 1014
- 43.92: 👍♦️ ✋ left.middle.tip (-0.08,-0.49,-0.12) ∆ thumbTip(-0.07,-0.46,-0.14) =>  0.039 👐1014 0 => 3
- 43.92: 👍♦️ ✋ left.ring.tip   (-0.10,-0.50,-0.13) ∆ thumbTip(-0.07,-0.46,-0.14) =>  0.051 👐1019 0 => 3
- 43.92: 👍♦️ ✋ left.little.tip (-0.11,-0.50,-0.13) ∆ thumbTip(-0.07,-0.46,-0.14) =>  0.061 👐1024 0 => 3
- 43.92: 🖐️ little.tip(-0.11,-0.50,-0.13) ring.tip(-0.10,-0.50,-0.13) index.tip(-0.06,-0.48,-0.13) middle.tip(-0.08,-0.49,-0.12) thumb.tip(-0.07,-0.46,-0.14) 
-
- 43.92: 👍♦️ 🤚 right.index.tip ( 0.03, 0.89,-0.23) ∆ thumbTip( 0.03, 0.87,-0.21) =>  0.035 👐2009 0 => 3
- 43.92: 👍♦️ 🤚 right.middle.tip( 0.05, 0.87,-0.23) ∆ thumbTip( 0.03, 0.87,-0.21) =>  0.030 👐2014 0 => 3
- 43.92: 👍♦️ 🤚 right.ring.tip  ( 0.07, 0.86,-0.24) ∆ thumbTip( 0.03, 0.87,-0.21) =>  0.050 👐2019 0 => 3
- 43.92: 👍♦️ 🤚 right.little.tip( 0.08, 0.85,-0.24) ∆ thumbTip( 0.03, 0.87,-0.21) =>  0.067 👐2024 0 => 3
-Session event dataProviderStateChanged(dataProviders: [HandTrackingProvider(0x0000000302b5cae0, state: running)], newState: running, error: nil)
- 43.92: 🧭 resizeFrame(2732,2048) clipNorm( 0.12, 0.00,  0.75, 1.00)
- 43.92: 🧭 Immersive size(910,682) ports:0
-cannot add handler to 0 from 1 - dropping
-Presenting a drawable without a device anchor. This drawable won't be presented.
- 43.95: draw -> slide -> color -> tile -> flat -> cube -> plato -> nil
- 44.65: 🧭 Immersive secondMenuFrame
- 44.65: 🧭 Immersive size(1355,1087) ports:2
- 44.97: 🧭 Immersive secondMenuFrame
- 44.97: 🧭 Immersive size(1355,1087) ports:2
- 45.93: 🖐️ index.tip( 0.03, 0.92,-0.22) little.tip( 0.08, 0.87,-0.24) thumb.tip( 0.02, 0.90,-0.21) ring.tip( 0.06, 0.88,-0.23) middle.tip( 0.04, 0.89,-0.22) 
-
- 47.94: 🖐️ index.tip( 0.03, 0.90,-0.22) little.tip( 0.08, 0.86,-0.23) thumb.tip( 0.02, 0.88,-0.21) ring.tip( 0.07, 0.86,-0.22) middle.tip( 0.05, 0.87,-0.21) 
-
- 47.96: draw -> slide -> color -> tile -> flat -> cube -> plato -> nil
- 48.94: 👍🟢 🤚 right.index.tip ( 0.04, 0.90,-0.22) ∆ thumbTip( 0.03, 0.89,-0.21) =>  0.014 👐2009 3 => 0
- 48.94: 🤚👍1
- 49.09: 👍♦️ 🤚 right.index.tip ( 0.04, 0.90,-0.23) ∆ thumbTip( 0.03, 0.89,-0.21) =>  0.018 👐2009 1 => 3
-ARPredictorRemoteService <0x111f5de60>: Query queue is not running.
-Session event dataProviderStateChanged(dataProviders: [HandTrackingProvider(0x0000000302b5cae0, state: paused)], newState: paused, error: nil)
- 49.14: 📋 updateUIView touchesView(0,0, 0,0)
- 49.15: 📋 updateUIView touchesView(0,0, 0,0)
- 49.15: 🧭 resizeFrame(2400,1800) clipNorm( 0.12, 0.00,  0.75, 1.00)
- 49.15: 🧭 Non-Immersive size(800,600) ports:2
- 49.21: 🧭 resizeFrame(2400,1800) clipNorm( 0.12, 0.00,  0.75, 1.00)
- 49.21: 🧭 Non-Immersive size(800,600) ports:2
-cannot add handler to 0 from 1 - dropping
-Info.plist configuration "(no name)" for CPSceneSessionRoleImmersiveSpaceApplication contained UISceneClassName key, but could not load class with name "".
-Info.plist configuration "(no name)" for CPSceneSessionRoleImmersiveSpaceApplication contained UISceneDelegateClassName key, but could not load class with name "".
-Info.plist configuration "(no name)" for CPSceneSessionRoleImmersiveSpaceApplication contained UISceneClassName key, but could not load class with name "".
-Info.plist configuration "(no name)" for CPSceneSessionRoleImmersiveSpaceApplication contained UISceneDelegateClassName key, but could not load class with name "".
-Session event dataProviderStateChanged(dataProviders: [HandTrackingProvider(0x0000000302b5cae0, state: running)], newState: running, error: nil)
- 51.78: 📋 updateUIView touchesView(0,0, 0,0)
- 51.80: 📋 updateUIView touchesView(0,0, 0,0)
- 51.80: 🧭 resizeFrame(4065,3263) clipNorm( 0.15, 0.00,  0.70, 1.00)
- 51.80: 🧭 Immersive size(1355,1087) ports:2
- 51.82: 🧭 Immersive config
-cannot add handler to 0 from 1 - dropping
- 51.85: 🧭 resizeFrame(4065,3263) clipNorm( 0.15, 0.00,  0.70, 1.00)
- 51.85: 🧭 Immersive size(1355,1087) ports:2
- 51.85: 🖐️ index.tip( 0.03, 0.90,-0.23) little.tip( 0.08, 0.85,-0.23) thumb.tip( 0.03, 0.88,-0.21) ring.tip( 0.07, 0.86,-0.23) middle.tip( 0.05, 0.87,-0.23) 
-
-cannot add handler to 0 from 1 - dropping
- 53.75: 👍🟢 🤚 right.index.tip ( 0.04, 0.90,-0.23) ∆ thumbTip( 0.04, 0.89,-0.22) =>  0.014 👐2009 3 => 0
- 53.75: 🤚👍1
- 53.86: 🖐️ index.tip( 0.04, 0.89,-0.23) little.tip( 0.09, 0.85,-0.23) thumb.tip( 0.04, 0.89,-0.22) ring.tip( 0.08, 0.86,-0.22) middle.tip( 0.06, 0.87,-0.22) 
-
- 53.94: 👍♦️ 🤚 right.index.tip ( 0.04, 0.90,-0.23) ∆ thumbTip( 0.04, 0.88,-0.22) =>  0.017 👐2009 1 => 3
-ARPredictorRemoteService <0x111f5de60>: Query queue is not running.
-Session event dataProviderStateChanged(dataProviders: [HandTrackingProvider(0x0000000302b5cae0, state: paused)], newState: paused, error: nil)
- 53.99: 📋 updateUIView touchesView(0,0, 0,0)
- 54.01: 📋 updateUIView touchesView(0,0, 0,0)
- 54.01: 🧭 resizeFrame(2400,1800) clipNorm( 0.12, 0.00,  0.75, 1.00)
- 54.01: 🧭 Non-Immersive size(800,600) ports:2
- 54.10: 🧭 resizeFrame(2400,1800) clipNorm( 0.12, 0.00,  0.75, 1.00)
- 54.10: 🧭 Non-Immersive size(800,600) ports:2
-cannot add handler to 0 from 1 - dropping
-Session event dataProviderStateChanged(dataProviders: [HandTrackingProvider(0x0000000302b5cae0, state: running)], newState: running, error: nil)
-Info.plist configuration "(no name)" for CPSceneSessionRoleImmersiveSpaceApplication contained UISceneClassName key, but could not load class with name "".
-Info.plist configuration "(no name)" for CPSceneSessionRoleImmersiveSpaceApplication contained UISceneDelegateClassName key, but could not load class with name "".
-Info.plist configuration "(no name)" for CPSceneSessionRoleImmersiveSpaceApplication contained UISceneClassName key, but could not load class with name "".
-Info.plist configuration "(no name)" for CPSceneSessionRoleImmersiveSpaceApplication contained UISceneDelegateClassName key, but could not load class with name "".
- 59.30: 🧭 Immersive config
-cannot add handler to 0 from 1 - dropping
- 59.34: 📋 updateUIView touchesView(0,0, 0,0)
- 59.35: 📋 updateUIView touchesView(0,0, 0,0)
- 59.35: 🧭 resizeFrame(4065,3263) clipNorm( 0.15, 0.00,  0.70, 1.00)
- 59.35: 🧭 Immersive size(1355,1087) ports:2
- 59.37: 🖐️ index.tip( 0.04, 0.90,-0.23) little.tip( 0.10, 0.84,-0.22) thumb.tip( 0.03, 0.88,-0.21) ring.tip( 0.08, 0.85,-0.22) middle.tip( 0.06, 0.87,-0.22) 
-
- 59.37: 🧭 resizeFrame(4065,3263) clipNorm( 0.15, 0.00,  0.70, 1.00)
- 59.37: 🧭 Immersive size(1355,1087) ports:2
-cannot add handler to 0 from 1 - dropping
- 61.37: 🖐️ index.tip( 0.04, 0.90,-0.23) little.tip( 0.10, 0.84,-0.22) thumb.tip( 0.04, 0.89,-0.21) ring.tip( 0.09, 0.85,-0.21) middle.tip( 0.07, 0.87,-0.21) 
-
- 63.38: 🖐️ index.tip( 0.05, 0.90,-0.23) little.tip( 0.11, 0.84,-0.21) thumb.tip( 0.04, 0.89,-0.21) ring.tip( 0.09, 0.85,-0.21) middle.tip( 0.07, 0.87,-0.21) 
-
- 63.66: 👍🟢 🤚 right.index.tip ( 0.05, 0.90,-0.23) ∆ thumbTip( 0.04, 0.89,-0.21) =>  0.015 👐2009 3 => 0
- 63.66: 🤚👍1
- 63.88: 👍♦️ 🤚 right.index.tip ( 0.05, 0.90,-0.23) ∆ thumbTip( 0.04, 0.89,-0.21) =>  0.015 👐2009 1 => 3
-ARPredictorRemoteService <0x111f5de60>: Query queue is not running.
-Session event dataProviderStateChanged(dataProviders: [HandTrackingProvider(0x0000000302b5cae0, state: paused)], newState: paused, error: nil)
- 63.89: 📋 updateUIView touchesView(0,0, 0,0)
- 63.91: 📋 updateUIView touchesView(0,0, 0,0)
- 63.91: 🧭 resizeFrame(2400,1800) clipNorm( 0.12, 0.00,  0.75, 1.00)
- 63.91: 🧭 Non-Immersive size(800,600) ports:2
- 64.00: 🧭 resizeFrame(2400,1800) clipNorm( 0.12, 0.00,  0.75, 1.00)
- 64.00: 🧭 Non-Immersive size(800,600) ports:2
-cannot add handler to 0 from 1 - dropping
+26.00: 👍♦️ 🤚 right.index.tip ( 0.06, 0.91,-0.23) ∆ thumbTip( 0.05, 0.87,-0.20) =>  0.048 👐2009 0 => 3
+26.00: 👍♦️ 🤚 right.middle.tip( 0.08, 0.92,-0.25) ∆ thumbTip( 0.05, 0.87,-0.20) =>  0.071 👐2014 0 => 3
+26.00: 👍♦️ 🤚 right.ring.tip  ( 0.11, 0.92,-0.26) ∆ thumbTip( 0.05, 0.87,-0.20) =>  0.095 👐2019 0 => 3
+26.00: 👍♦️ 🤚 right.little.tip( 0.13, 0.89,-0.26) ∆ thumbTip( 0.05, 0.87,-0.20) =>  0.100 👐2024 0 => 3
+26.00: 👍♦️ ✋ left.index.tip  (-0.01, 0.67,-0.19) ∆ thumbTip(-0.01, 0.68,-0.16) =>  0.032 👐1009 0 => 3
+26.00: 👍♦️ ✋ left.middle.tip (-0.01, 0.65,-0.18) ∆ thumbTip(-0.01, 0.68,-0.16) =>  0.036 👐1014 0 => 3
+26.00: 👍♦️ ✋ left.ring.tip   (-0.03, 0.64,-0.18) ∆ thumbTip(-0.01, 0.68,-0.16) =>  0.050 👐1019 0 => 3
+26.00: 👍♦️ ✋ left.little.tip (-0.05, 0.63,-0.18) ∆ thumbTip(-0.01, 0.68,-0.16) =>  0.069 👐1024 0 => 3
+28.74: 👍🟢 🤚 right.index.tip ( 0.07, 1.01,-0.32) ∆ thumbTip( 0.06, 1.00,-0.31) =>  0.014 👐2009 3 => 0
+28.74: 🤚👍1
+29.29: 👍♦️ 🤚 right.index.tip ( 0.06, 1.02,-0.31) ∆ thumbTip( 0.05, 1.00,-0.31) =>  0.019 👐2009 1 => 3
+29.69: 👍🟢 🤚 right.middle.tip( 0.07, 1.01,-0.32) ∆ thumbTip( 0.06, 1.00,-0.32) =>  0.012 👐2014 3 => 0
+30.18: 👍♦️ 🤚 right.middle.tip( 0.08, 1.01,-0.33) ∆ thumbTip( 0.06, 1.00,-0.32) =>  0.019 👐2014 1 => 3
+30.60: 👍🟢 🤚 right.ring.tip  ( 0.08, 1.01,-0.32) ∆ thumbTip( 0.07, 1.01,-0.31) =>  0.014 👐2019 3 => 0
+31.08: 👍♦️ 🤚 right.ring.tip  ( 0.08, 1.01,-0.33) ∆ thumbTip( 0.07, 1.00,-0.31) =>  0.017 👐2019 1 => 3
+31.59: 👍🟢 🤚 right.little.tip( 0.09, 1.01,-0.32) ∆ thumbTip( 0.08, 1.01,-0.31) =>  0.013 👐2024 3 => 0
+31.88: 👍♦️ 🤚 right.little.tip( 0.09, 1.00,-0.32) ∆ thumbTip( 0.07, 1.00,-0.31) =>  0.015 👐2024 1 => 3
+31.92: 👍🟢 🤚 right.little.tip( 0.09, 1.00,-0.32) ∆ thumbTip( 0.07, 1.00,-0.31) =>  0.015 👐2024 3 => 0
+32.05: 👍♦️ 🤚 right.little.tip( 0.09, 1.00,-0.32) ∆ thumbTip( 0.07, 1.00,-0.31) =>  0.018 👐2024 1 => 3
+32.48: 👍🟢 ✋ left.index.tip  (-0.08, 0.99,-0.31) ∆ thumbTip(-0.07, 0.99,-0.30) =>  0.014 👐1009 3 => 0
+32.75: ✋👍1
+32.98: 👍♦️ ✋ left.index.tip  (-0.08, 1.00,-0.30) ∆ thumbTip(-0.07, 1.00,-0.29) =>  0.016 👐1009 1 => 3
+33.33: 👍🟢 ✋ left.middle.tip (-0.10, 0.99,-0.32) ∆ thumbTip(-0.09, 0.99,-0.31) =>  0.015 👐1014 3 => 0
+33.87: 👍♦️ ✋ left.middle.tip (-0.10, 0.99,-0.33) ∆ thumbTip(-0.08, 0.98,-0.31) =>  0.019 👐1014 1 => 3
+34.69: 👍🟢 ✋ left.ring.tip   (-0.10, 0.99,-0.32) ∆ thumbTip(-0.10, 0.99,-0.31) =>  0.015 👐1019 3 => 0
+34.76: 👍♦️ ✋ left.ring.tip   (-0.11, 0.99,-0.32) ∆ thumbTip(-0.10, 0.99,-0.31) =>  0.015 👐1019 1 => 3
+35.35: 👍🟢 ✋ left.little.tip (-0.10, 0.98,-0.31) ∆ thumbTip(-0.10, 0.99,-0.31) =>  0.014 👐1024 3 => 0
+35.82: 👍♦️ ✋ left.little.tip (-0.11, 0.98,-0.31) ∆ thumbTip(-0.10, 0.98,-0.31) =>  0.015 👐1024 1 => 3
+35.95: 👍🟢 ✋ left.little.tip (-0.11, 0.98,-0.31) ∆ thumbTip(-0.10, 0.98,-0.31) =>  0.014 👐1024 3 => 0
+36.02: 👍♦️ ✋ left.little.tip (-0.12, 0.99,-0.32) ∆ thumbTip(-0.10, 0.99,-0.31) =>  0.018 👐1024 1 => 3
+37.77: 👍🟢 🤚 right.index.tip ( 0.14, 0.98,-0.25) ∆ thumbTip( 0.13, 0.96,-0.24) =>  0.013 👐2009 3 => 0
+37.77: 🤚👍1
+37.91: 👍♦️ 🤚 right.index.tip ( 0.14, 0.98,-0.25) ∆ thumbTip( 0.13, 0.96,-0.24) =>  0.016 👐2009 1 => 3
