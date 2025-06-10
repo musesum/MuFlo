315f8974fba46b39ab95d78c19cc17e73b403354
musesum <musesum@gmail.com>
2025-06-10 15:52:10 -0700

    sync archive peers

---
+++ Sources/MuFlo/Archive/ArchiveFrame.swift
@@ -0,0 +1,16 @@
+// created by musesum on 6/10/25
+
+import Foundation
+
+/// Codable struct for sharing archive data between peers
+public struct ArchiveFrame: Codable {
+    public let url: URL
+    public let data: Data
+    public let name: String
+    
+    public init(url: URL, data: Data) {
+        self.url = url
+        self.data = data
+        self.name = url.deletingPathExtension().lastPathComponent
+    }
+}
