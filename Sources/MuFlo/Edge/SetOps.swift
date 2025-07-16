78336c768ac890e5ffb5a6e88f7d6aa80abe4413
musesum <musesum@gmail.com>
2025-07-16 09:47:53 -0700

    Tree menu fade

--- Sources/MuFlo/Edge/SetOps.swift
+++ Sources/MuFlo/Edge/SetOps.swift
@@ -6,7 +6,7 @@ import Foundation
 
 public struct SetOps: OptionSet, Sendable {
 
-    nonisolated(unsafe) public static let fire    = SetOps(rawValue: 1 << 0) // trigger event
+    public static let fire    = SetOps(rawValue: 1 << 0) // trigger event
     public static let sneak   = SetOps(rawValue: 1 << 1) // quietly set value, no trigger
     public static let changed = SetOps(rawValue: 1 << 3) // bang only when changed
     public static let ranging = SetOps(rawValue: 1 << 4) // reset range for m~n
